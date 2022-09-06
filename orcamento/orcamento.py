52b2893ddddc8049ac7a7c2fde7060c1df0a8af0
MarcosRos4 <103190249+MarcosRos4@users.noreply.github.com>
2022-09-05 22:27:17 -0300

    Update orcamento.py

--- orcamento/orcamento.py
+++ orcamento/orcamento.py
@@ -15,30 +15,21 @@ orc_frm.pack()
 
 # puxando a tabela
 # escolher qual tabela usar
-def pesquisar(event=None):
-    arquivo = filedialog.askopenfilename(initialdir="./orcamento",
-                  title="Escolha uma tabela de orçamento",
-                  filetypes=(("Excel files", "*.xlsx*"),("all files", "*.*")))
-    psq_lbl['text'] = "Aquivo escolhido:\n " + arquivo
-    orc_df = pd.read_excel(arquivo)
-    #print(orc_df)
 
-psq_btn = tk.Button(master=orc_frm, font="MsComicSans 14", text="Procurar Tabela")
-psq_btn.bind("<Button>", pesquisar)
-psq_btn.place(x=50, y=130)
+arquivo = filedialog.askopenfilename(initialdir="./orcamento",
+              title="Escolha uma tabela de orçamento",
+              filetypes=(("Excel files", "*.xlsx*"),("all files", "*.*")))
+orc_df = pd.read_excel(arquivo)
+print(orc_df)
 
 psq_lbl = tk.Label(master=orc_frm, font="MsComicSans 14")
+psq_lbl['text'] = "Aquivo escolhido:\n " + arquivo
 psq_lbl.place(x=50, y=80)
-
-# ler a tabela no python
-
-
 
 # classe de acomodações
 # master, cord de btn, capacidade, valor, imagem, pessoas, 
 class Acomod:
-    def __init__(self, nome, quantidade, capacidade, valor, cordX, cordY, ocupacao, cafe=0):
-        self.cafe = cafe
+    def __init__(self, nome, quantidade, capacidade,cordX, cordY, ocupacao):
         self.capacidade = capacidade
         self.cordX = cordX
         self.cordY = cordY
@@ -46,7 +37,7 @@ class Acomod:
         self.nome = nome
         self.ocupacao = ocupacao
         self.quantidade = quantidade 
-        self.valor = valor    
+        self.valor = 0   
 
         # label de ocupação
         self.kpc = tk.Label(master=orc_frm, text=f'Quantidade: {self.quantidade} \n Capacidade: {self.capacidade} \n Ocupação: {self.ocupacao}', font='Arial 14')
@@ -74,16 +65,15 @@ class Acomod:
             return 0
 
     def custo(self):
-        
         if(self.check()==1):
             return '---------'
         else:
             if (self.ocupacao == 1):
-                print('valor total de 220')
+                self.valor =  orc_df[self.nome][0]
             elif ( self.ocupacao == 2):
-                print('valor total de 290')
+                self.valor = orc_df[self.nome][1]
             else:
-                print(f'valor total de [duplo + cama extra * {self.ocupacao - 2}]')
+                self.valor = (orc_df[self.nome][1] + (orc_df[self.nome][2] * (self.ocupacao - 2)))
     
     def paste(self):
         copypasta = f"_*{self.nome}*_\nR$ {self.custo()} (valor de 24h)\nR${self.custo()*1.9} (valor de 48h)\n"
@@ -97,53 +87,65 @@ class Acomod:
             self.btn['bg']='white'
             self.estado=False
 
+    def aumenta(self, event=None):
+        if (self.estado==True):
+            self.ocupacao+=1
+            self.check()
+            self.custo()
+            self.kpc['text'] = f'Quantidade: {self.quantidade} \n Capacidade: {self.capacidade} \n Ocupação: {self.ocupacao}'
+            self.tot['text'] = f'Total R$:{self.valor}'
+            
+    def diminui(self, event=None):
+        if (self.estado==True):
+            if(self.ocupacao == 0):
+                pass
+            else:
+                self.ocupacao-=1
+                self.check()
+                self.custo()
+                self.kpc['text'] = f'Quantidade: {self.quantidade} \n Capacidade: {self.capacidade} \n Ocupação: {self.ocupacao}'
+                self.tot['text'] = f'Total R$:{self.valor}'
+
 # criando as acomod
-suit = Acomod('Suíte', 1, 10, 10.00, 400 , 250, 0)
+# parametros nome, quantidade, capacidade,cordX, cordY, ocupacao
+suit = Acomod('suit', 1, 10, 400 , 250, 0)
 
-apto = Acomod('Apartamento', 1, 5, 10.00, 600, 250, 0)
+apto = Acomod("apto", 1, 5, 600, 250, 0)
 
-cabn = Acomod('Cabana Americana', 1, 3, 10.00, 800, 250, 0)
+cabn = Acomod('cabn', 1, 3, 800, 250, 0)
 
-casa = Acomod('Casarão', 1, 5, 10.00, 1000, 250, 0)
+casa = Acomod('casa', 1, 5, 1000, 250, 0)
 
-swis = Acomod('Suíço', 1, 3, 10.00, 1200, 250, 0)
+swis = Acomod('swis', 1, 3, 1200, 250, 0)
 
-stan = Acomod('Standart', 1, 8, 10.00, 1400, 250, 0)
+stan = Acomod('stan', 1, 8, 1400, 250, 0)
+
 
 # icone label
 ico_lbl = tk.Label(orc_frm, font='MsComicSans 14', text='PESSOA')
 ico_lbl.place(x=870, y=550)
 
 #alterar o numero de pessoas na acomod
-def aumenta(event=None):
-    lista = [suit, apto, cabn, casa, swis, stan]
-    for i in lista:
-        if i.estado==True:
-            i.ocupacao+=1
-            i.check()
-            i.kpc['text'] = f'Quantidade: {i.quantidade} \n Capacidade: {i.capacidade} \n Ocupação: {i.ocupacao}'
-            i.tot['text'] = f'Total R$:{i.custo()}'
-            
-def diminui(event=None):
-    lista = [suit, apto, cabn, casa, swis, stan]
-    for i in lista:
-        if i.estado==True:
-            if(i.ocupacao == 0):
-                pass
-            else:
-                i.ocupacao-=1
-            i.check()
-            i.kpc['text'] = f'Quantidade: {i.quantidade} \n Capacidade: {i.capacidade} \n Ocupação: {i.ocupacao}'
-            i.tot['text'] = f'Total R$:{i.custo()}'
+
 
 # adicionar pessoas
 mais_btn = tk.Button(orc_frm, font='MsComicSans 14',  text='+')
-mais_btn.bind('<Button>', aumenta)
+mais_btn.bind('<Button>', suit.aumenta, add="+")
+mais_btn.bind('<Button>', apto.aumenta, add="+")
+mais_btn.bind('<Button>', cabn.aumenta, add="+")
+mais_btn.bind('<Button>', swis.aumenta, add="+")
+mais_btn.bind('<Button>', casa.aumenta, add="+")
+mais_btn.bind('<Button>', stan.aumenta, add="+")
 mais_btn.place(x=950, y=550)
 
 # subtrair pessoas
 mens_btn = tk.Button(orc_frm, font='MsComicSans 14', text='-')
-mens_btn.bind('<Button>', diminui)
+mens_btn.bind('<Button>', suit.diminui, add="+")
+mens_btn.bind('<Button>', apto.diminui, add="+")
+mens_btn.bind('<Button>', cabn.diminui, add="+")
+mens_btn.bind('<Button>', swis.diminui, add="+")
+mens_btn.bind('<Button>', casa.diminui, add="+")
+mens_btn.bind('<Button>', stan.diminui, add="+")
 mens_btn.place(x=850, y=550)
 
 # copy to clipboard
@@ -169,8 +171,6 @@ def config(event=None):
     cfg_win.maxsize(1920, 1080)
     cfg_frm = tk.Frame(master=cfg_win, height=1080, width=1920)
     
-
-
 # botão para as configurações
 cfg_btn = tk.Button(orc_frm, font='MsComicSans 14', text='Configurações')
 cfg_btn.bind('<Button>', config)
@@ -179,6 +179,5 @@ cfg_btn.place(x=1750, y=0)
 # janela das configurações
 
 # frame das configurações
-
 
 janela.mainloop()
